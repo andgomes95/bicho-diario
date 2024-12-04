295769377bdf78517032183b8015096fb5354115
Andre Gomes <andgomes95@gmail.com>
2024-12-03 22:29:18 -0300

    Adicionando formulario inicial

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
 function App() {
+  const [formData, setFormData] = useState({
+    animalHere: ''
+  })
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();  // Evita o comportamento padrão de recarregar a página
+    // Aqui você pode processar os dados ou fazer a chamada API
+    console.log(formData);
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Bicho Diario</h1>
+        <form onSubmit={handleSubmit}>
+          <input 
+            name="animalHere"
+            id="animalHere"
+            type="text" 
+            placeholder='Qual sera o Bicho de hoje?'
+            value={formData.animalHere}
+            onChange={handleChange}
+          />
+          <button type="submit">Enviar</button>
+        </form>
       </header>
     </div>
   );
